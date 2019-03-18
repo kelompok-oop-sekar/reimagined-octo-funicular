dfcd8969db205a62c26e95f11a97fc6a196849ca
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 21:07:10 +0700

    Update DuckEgg.h

--- Product/DuckEgg.h
+++ Product/DuckEgg.h
@@ -1,15 +1,17 @@
+//Nama File : DuckEgg.h
+
 #ifndef DUCKEGG_H
 #define DUCKEGG_H
 
 #include "Product.h"
-
+//kelas DuckEgg adalah nama kelas dari objek objek telur bebek
 class DuckEgg : public Product{
 private:
-	static int n;
-	static int price;
+	static int n; //Jumlah dari DuckEgg yang dimiliki pemain saat ini
+	static int price; //Harga DuckEgg setiap satuannya
 public:
-	static int getNProduct();
-	static int getPrice();
+	static int getNProduct(); //Mengembalikan jumlah dari telur bebek yang dimiliki oleh pemain
+	static int getPrice(); //mengembalikan harga dari telur bebek setiap satuannya
 	~DuckEgg();
 };
 
