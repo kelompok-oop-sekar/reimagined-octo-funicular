7a7eb3728384f058663857fd6f014b4eebf2bb02
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 20:41:41 +0700

    Update BuffaloMeat.h

--- Product/BuffaloMeat.h
+++ Product/BuffaloMeat.h
@@ -1,15 +1,17 @@
+// Nama File = BuffaloMeat.h
+
 #ifndef BUFFALOMEAT_H
 #define BUFFALOMEAT_H
 
 #include "Product.h"
-
+//BuffaloMeat adalah class dari objek daging kerbau yang bisa didapat player saat bermain
 class BuffaloMeat : public Product{
 private:
-	static int n;
-	static int price;
+	static int n; // jumlah buffalomeat yang dimiliki player
+	static int price; // harga buffalomeat
 public:
-	static int getNProduct();
-	static int getPrice();
+	static int getNProduct(); //mengembalikan jumlah buffalomeat yang dimiliki player
+	static int getPrice(); // mengembalikan harga buffalomeat jika terjual
 	~BuffaloMeat();
 };
 
