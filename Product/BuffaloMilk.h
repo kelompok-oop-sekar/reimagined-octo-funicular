bb7415981cd401b59520d90b1fe42be958848168
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 20:43:50 +0700

    Update BuffaloMilk.h

--- Product/BuffaloMilk.h
+++ Product/BuffaloMilk.h
@@ -1,15 +1,17 @@
+// NamaFile BUffaloMilk.h
+
 #ifndef BUFFALOMILK_H
 #define BUFFALOMILK_H
 
 #include "Product.h"
-
+// Kelas BuffaloMilk adalah kelas dari susu kerbau yang bisa didapat pemain saat program dijalankan
 class BuffaloMilk : public Product{
 private:
-	static int n;
-	static int price;
+	static int n; //Mengembalikan jumlah dari BuffaloMilk yang dimiliki oleh pemain
+	static int price; //Mengembalikan harga dari BuffaloMilk saat penjualan
 public:
-	static int getNProduct();
-	static int getPrice();
+	static int getNProduct(); //mengembalikan jumlah buffalomeat yang dimiliki player
+	static int getPrice(); // mengembalikan harga buffalomeat jika terjual
 	~BuffaloMilk();
 };
 
