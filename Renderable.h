9ca54af9f5aa4b4fe39f709060e5c6fa9592c7d3
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 21:24:06 +0700

    Update Renderable.h

--- Renderable.h
+++ Renderable.h
@@ -1,12 +1,14 @@
+//Nama file : Renderable.h
+
 #ifndef RENDERABLE_H
 #define RENDERABLE_H
-
+//Renderable merupakan ABC untuk kelas player, cell, dan farmanimal
 class Renderable {
 protected:
-	static char** Map;
+	static char** Map; // Map merupakan tempat menyimpan karakter hasil dari Renderable objek sesuai koordinatnya
 
 public:
-	virtual char render() = 0;
+	virtual char render() = 0; //method untuk mengembalikan karakter sesuai dari perilaku instantiasi renderable objects
 };
 
 #endif
