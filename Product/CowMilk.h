153ca8581eb2f69a0cdd9de63e3305da4f2f6422
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 21:02:41 +0700

    Update CowMilk.h

--- Product/CowMilk.h
+++ Product/CowMilk.h
@@ -1,15 +1,17 @@
+//Nama File : CowMilk.h
+
 #ifndef COWMILK_H
 #define COWMILK_H
 
 #include "Product.h"
-
+//CowMilk merupakan nama kelas dari objek susu sapi yang bisa didapat pemain saat bermain
 class CowMilk : public Product{
 private:
-	static int n;
-	static int price;
+	static int n; //jumlah dari CowMilk yang dimiliki oleh pemain
+	static int price; //harga dari CowMilk setiap satuannya 
 public:
-	static int GetNProduct();
-	static int GetPrice();
+	static int GetNProduct(); //mengembalikan jumlah dari CowMilk yang dimiliki pemain
+	static int GetPrice(); //mengembalikan harga dari CowMilk setiap satuannya
 	~CowMilk();
 };
 
