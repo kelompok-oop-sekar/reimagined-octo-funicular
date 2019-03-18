570f5aeb147e5d4dcd7a2589477b6bc2aea8398a
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 20:50:45 +0700

    Update ChickenEgg.h

--- Product/ChickenEgg.h
+++ Product/ChickenEgg.h
@@ -1,15 +1,17 @@
+//Namafile : ChickenEgg.h
+
 #ifndef CHICKENEGG_H
 #define CHICKENEGG_H
 
 #include "Product.h"
-
+//ChickenEgg adalah kelas dari telur ayam yang bisa didapat pemain dari ayam
 class ChickenEgg : public Product{
 private:
-	static int n;
-	static int price;
+	static int n; //jumlah dari chickenegg yang sedang dimiliki oleh pemain
+	static int price; //harga jual setiap satu chickenegg
 public:
-	static int GetNProduct();
-	static int GetPrice();
+	static int GetNProduct(); //mengembalikan jumlah dari chickenegg yang sedang dimiliki oleh pemain
+	static int GetPrice(); //mengembalikan jumlah harga yang didapat pada saat penjualan chickenegg
 	~ChickenEgg();
 };
 
