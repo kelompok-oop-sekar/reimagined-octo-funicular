cb320c61c2be2c632d151f330d501873c4dc1968
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 21:20:31 +0700

    Update XXNormalChicken.h

--- Product/XXNormalChicken.h
+++ Product/XXNormalChicken.h
@@ -1,17 +1,19 @@
+//Nama File : XXNormalChicken.h
+
 #ifndef XXNORMALCHICKEN_H
 #define XXNORMALCHICKEN_H
 
 #include "Product.h"
-
+//XXNormalChicken adalah nama kelas dari makanan yang terinspirasi dari menu ayam penyet XXNormal yang terdiri dari telur dan ayam
 class XXNormalChicken : public Product{
 private:
-	static string ingredients[1] = { "ChickenMeat", "ChickenEgg" };
-	static int n;
-	static int price;
+	static string ingredients[2] = { "ChickenMeat", "ChickenEgg" }; // komposisi dari kelas makanan ini
+	static int n; // mengembalikan jumlah dari objek kelas XXNormalChicken yang dimiliki pemain
+	static int price; //harga dari XXNormalProduct setiap sajiannya
 public:
-	static int getNProduct();
-	static int getPrice();
-	static string* getIngredients();
+	static int getNProduct(); //mengembalikan jumlah dari XXNormalChicken yang dimiliki pemain
+	static int getPrice(); //mengembalikan harga dari XXNormalChicken setiap sajiannya
+	static string* getIngredients(); //Mengembalikan komposisi dari XXNormalChicken
 };
 
 #endif
