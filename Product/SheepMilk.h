e14a63069252bea6c9f9a3be5de14b54ba22a869
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 21:15:57 +0700

    Update SheepMilk.h

--- Product/SheepMilk.h
+++ Product/SheepMilk.h
@@ -1,16 +1,18 @@
+//Nama file : SheepMilk.h
+
 #ifndef SHEEPMILK_H
 #define SHEEPMILK_H
 
 #include "Product.h"
-
+//SheepMilk adalah nama kelas dari objek objek susu domba yang bisa dimilki pemain saat permainan
 class SheepMilk : public Product{
 private:
-	static int n;
-	static int price;
+	static int n;//Jumlah dari sheepmilk yang dimiliki pemain saat permainan
+	static int price;//Harga dari sheepmilk setiap satuannya
 public :
-	static int getNProduct();
-	static int getPrice();
-	¬SheepMilk;
+	static int getNProduct();//Mengembalika jumlah dari sheepmilk yang dibutuhkan
+	static int getPrice();//Mengembalikan harga dari sheepmilk setiap satuannya
+	Â¬SheepMilk;
 };
 
 #endif
