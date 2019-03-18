744971c30dea51c196861d7e999acde5a31181de
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 21:11:08 +0700

    Update Product.h

--- Product/Product.h
+++ Product/Product.h
@@ -1,13 +1,15 @@
+//Nama file : Product.h
+
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
 #include <string>
-
+//Product adalah base class dari produk produk yang bisa didapat pemain, farmproduct ataupun sideproduct
 class Product {
 private:
-	static int n;
+	static int n; //Jumlah dari keseluruhan produk yang dimiliki pemain di inventori
 public:
-	static int getNAllProduct();
+	static int getNAllProduct(); //Mengembalikan jumlah dari produk yang dimiliki pemain
 };
 
 #endif
