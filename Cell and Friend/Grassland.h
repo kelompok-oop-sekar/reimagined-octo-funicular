80cd8c767bf070eb7fccebae221dad81855ccb36
AbdurrahmanAdni <aadni79@gmail,com>
2019-03-18 15:09:17 +0700

    Coba

---
+++ Cell and Friend/Grassland.h
@@ -0,0 +1,20 @@
+#ifndef _GRASSLAND_H_
+#define _GRASSLAND_H_
+
+#include "Land.h"
+
+//Grassland Merupakan Turunan dari Land
+class Grassland : public Land {
+	public :
+		Grassland(int _x, int _y, bool _grass); 
+		/*Ctor dengan parameter. 
+		 * _x merepresentasikan posisi Grassland dalam sumbu x (Horizontal), 
+		 * _y merepresentasikan posisi Grassland dalam sumbu y (vertikal) 
+		 * _grass merepresentasikan apakah Grassland merupakan Land berumput atau tidak */
+		char render();// Hasil render adalah "-" jika tidak berumput, "#" jika berumput
+		bool isWithGrass();// Menghasilkan true jika Grassland berumput
+		void removeGrass();// Menghapus rumput. Mengembalikan "#" menjadi "-"
+		void setBerumput(bool _berumput); //Set rumput pada Grassland
+};
+
+#endif
---
+++ Timer.h
@@ -0,0 +1,9 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+class Timer{
+	public:
+		static int tick; //tick sebagai pengatur waktu dari game. Inisialisasi awal, tick = 1;
+};
+
+#endif
