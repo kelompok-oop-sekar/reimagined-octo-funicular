fa660c0e977cf41e3d03cfc655e0581b26525ab4
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 20:59:49 +0700

    Update CowMeat.h

--- Product/CowMeat.h
+++ Product/CowMeat.h
@@ -1,15 +1,17 @@
+//Nama file : CowMeat.h
+
 #ifndef COWMEAT_H
 #define COWMEAT_H
 
 #include "Product.h"
-
+//Kelas CowMeat adalah kelas dari objek objek daging sapi yang bisa didapat pemain saat bermain
 class CowMeat : public Product{
 private:
-	static int n;
-	static int price;
+	static int n; //jumlah dari daging sapi yang dimiliki oleh pemain
+	static int price; //harga dari penjualan dagin sapi setiap satuannya
 public:
-	static int getNProduct();
-	static int getPrice();
+	static int getNProduct(); //mengembalikan jumlah dari daging sapi yang dimiliki pemain
+	static int getPrice(); //mengembalikan harga dari dagin sapi saat penjualan
 	~CowMeat();
 };
 
