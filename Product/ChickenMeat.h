f8d74811cf6e265f63a687aae0928b177c62e2bd
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-03-18 20:53:26 +0700

    Update ChickenMeat.h

--- Product/ChickenMeat.h
+++ Product/ChickenMeat.h
@@ -1,15 +1,17 @@
+// Nama file : ChickenMeat.h
+
 #ifndef CHICKENMEAT_H
 #define CHICKENMEAT_H
 
 #include "Product.h"
-
+//ChickenMeat adalah nama kelas dari objek dagin ayam yang dapat didapat saat permainan
 class ChickenMeat : public Product {
 private:
-	static int n;
-	static int price;
+	static int n; //jumlah dari daging ayam yang dimiliki pemain
+	static int price; //harga dari daging ayam jika dijual
 public:
-	static int getNProduct();
-	static int getPrice();
+	static int getNProduct(); //mengembalikan jumlah telur ayam yang sedang dimiliki oleh pemain
+	static int getPrice(); //mengembalikan harga dari telur ayam
 	~ChickenMeat();
 };
 
