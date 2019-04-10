08a90cd343d34af34872e1394d93e1c8f3b3207b
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-04-10 23:57:55 +0700

    Add files via upload

--- Product/Product.h
+++ Product/Product.h
@@ -11,9 +11,10 @@ class Product {
 private:
 	static int n; //Jumlah dari keseluruhan produk yang dimiliki pemain di inventori
 public:
-	virtual string GetClassName() {}
-	virtual int getNAllProduct() {} //Mengembalikan jumlah dari produk yang dimiliki pemain
-	virtual string getIngredients(int i) {}
+	virtual string getClassName() { return ""; }
+	virtual int getNAllProduct() { return 0; } //Mengembalikan jumlah dari produk yang dimiliki pemain
+	virtual string getIngredients(int i) { return ""; }
+	virtual int getPrice() { return 0; }
 
 };
 
