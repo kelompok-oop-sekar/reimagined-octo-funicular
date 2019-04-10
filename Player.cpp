866eecbb0409f8886033e6fc00de4837d5024961
mnobelf <38283013+mnobelf@users.noreply.github.com>
2019-04-10 23:15:01 +0700

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -1,9 +1,6 @@
 #include "Player.h"
 #include <iostream>
-#include <string.h>
-#include <stdio.h>
-#include <conio.h>
-
+#include <map>
 using namespace std;
 
 // ctor dengan kapasitas wadah air default.
@@ -34,47 +31,39 @@ Player::Player(int capacity, int coin) {
 // 'd' = right
 void Player::move(char direction) {
 	if (direction == 'w') {
-		if (y == 0) {
+		if (x == 0) {
 			cout << "Player has reached top boundary" << endl;
 		}
 		else {
-			if (!isObjectExist(x, y - 1)) {
-				y--;
-				cout << "Player moves up" << endl;
-			}
+			x--;
+			cout << "Player moves up" << endl;
 		}
 	}
 	else if (direction == 'a') {
-		if (x == 0) {
+		if (y == 0) {
 			cout << "Player has reached left boundary" << endl;
 		}
 		else {
-			if (!isObjectExist(x - 1, y)) {
-				x--;
-				cout << "Player moves left" << endl;
-			}
+			y--;
+			cout << "Player moves left" << endl;
 		}
 	}
 	else if (direction == 's') {
-		if (y == 7) {
+		if (x == 8) {
 			cout << "Player has reached bottom boundary" << endl;
 		}
 		else {
-			if (!isObjectExist(x, y + 1)) {
-				y++;
-				cout << "Player moves down" << endl;
-			}
+			x++;
+			cout << "Player moves down" << endl;
 		}
 	}
 	else if (direction == 'd') {
-		if (x == 7) {
+		if (y == 8) {
 			cout << "Player has reached right boundary" << endl;
 		}
 		else {
-			if (!isObjectExist(x + 1, y)) {
-				x++;
-				cout << "Player moves right" << endl;
-			}
+			y++;
+			cout << "Player moves right" << endl;
 		}
 	}
 	else {
@@ -83,65 +72,8 @@ void Player::move(char direction) {
 }
 
 // Berbicara dengan hewan.
-void Player::talk(LinkedList<FarmAnimal*> List, int direction) {
-	/* direction :
-		1 -> atas
-		2 -> kanan
-		3 -> bawah
-		4 -> kiri
-	*/
-
-	// cari animal yang posisinya di dekat player
-
-	// -- Kasus untuk direction
-	int deltaX = 0;
-	int deltaY = 0;
-
-	switch (direction) {
-		case 1 : {
-			deltaY = -1;
-		}
-		break;
-		case 2 : {
-			deltaX = 1;
-		}
-		break;
-		case 3 : {
-			deltaY = 1;
-		}
-		break;
-		case 4 : {
-			deltaX = -1;
-		}
-		break;
-		default : {
-			// do nothing
-		}
-	}
-
-	// -- Player position
-	int xPos = this->getX();
-	int yPos = this->getY();
-
-	int i = 0;
-	// -- ukuran List
-	int size = List.getSize();
-	// -- boolean found animal terdekat player
-	bool found = false;
-
-	while (i<size && !found) {
-		int xAnimalPos = List.get(i)->getX();
-		int yAnimalPos = List.get(i)->getY();
-		if ((xAnimalPos == xPos+deltaX) && (yAnimalPos == yPos+deltaY)) {
-			found = true;
-		} else {
-			i++;
-		}
-	}
-
-	if (found) {
-		List.get(i)->sounding();
-	}
+void Player::talk(FarmAnimal& animal) {
+	animal.sounding();
 }
 
 // Interaksi Player:
@@ -174,85 +106,8 @@ void Player::interact(Truck& truck){
 
 // Menyembelih hewan "MeatProducingFarmAnimal" untuk mendapatkan
 // "Product" daging.
-void Player::kill(LinkedList<FarmAnimal*> List, int direction) {
-	/* direction :
-		1 -> atas
-		2 -> kanan
-		3 -> bawah
-		4 -> kiri
-	*/
-
-	// cari animal yang posisinya di dekat player
-
-	// -- Kasus untuk direction
-	int deltaX = 0;
-	int deltaY = 0;
-
-	switch (direction) {
-		case 1 : {
-			deltaY = -1;
-		}
-		break;
-		case 2 : {
-			deltaX = 1;
-		}
-		break;
-		case 3 : {
-			deltaY = 1;
-		}
-		break;
-		case 4 : {
-			deltaX = -1;
-		}
-		break;
-		default : {
-			// do nothing
-		}
-	}
-	// -- Player position
-	int xPos = this->getX();
-	int yPos = this->getY();
-
-	int i = 0;
-	// -- ukuran List
-	int size = List.getSize();
-	// -- boolean found animal terdekat player
-	bool found = false;
-
-	while ((i<size) && (!found)) {
-		int xAnimalPos = List.get(i)->getX();
-		int yAnimalPos = List.get(i)->getY();
-		if ((xAnimalPos == xPos+deltaX) && (yAnimalPos == yPos+deltaY)) {
-			found = true;
-		} else {
-			i++;
-		}
-	}
-	
-	if (found) {
-		cout << "Player gained meat" << endl;
-		string animalClassName;
-		animalClassName = List.get(i)->getClassName();
-		// mendeteksi kelas riil animal
-		if (strcmp(animalClassName, "Buffalo") == 0) {
-			BuffaloMeat* result = new BuffaloMeat();
-		} else if (strcmp(animalClassName, "Chicken") == 0) {
-			ChickenMeat* result = new ChickenMeat();
-		} else if (strcmp(animalClassName, "Cow") == 0) {
-			CowMeat* result = new CowMeat();
-		} else if (strcmp(animalClassName, "Duck") == 0) {
-			// DuckMeat* result = new DuckMeat();
-		} else if (strcmp(animalClassName, "Goat") == 0) {
-			GoatMeat* result = new GoatMeat();
-		} else if (strcmp(animalClassName, "Sheep") == 0) {
-			SheepMeat* result = new SheepMeat();
-		}
-		// menambahkan result ke inventory player
-		this->inventory.add(result);
-		// menghapus animal dari List animal
-		delete List.get(i);
-		List.remove(i);
-	}
+void Player::kill(MeatProducingFarmAnimal& animal) {
+	cout << "Player gained meat" << endl;
 }
 
 // Menyiram "Land" dengan wadah air yang dimiliki dan 
@@ -270,7 +125,7 @@ void Player::control() {
 		move(input);
 	}
 	else if (input == 'i') {
-		seeInventory();
+		//do something here
 	}
 	else {
 		cout << "Invalid input" << endl;
@@ -303,15 +158,4 @@ void Player::printCan() {
 
 void Player::printMoney() {
 	cout << "Money : " << money << endl;
-}
-
-void Player::seeInventory() {
-	if (inventory.isEmpty()) {
-		cout << "No items in inventory" << endl;
-	}
-	else {
-		for (int i = 0; i < inventory.getSize(); i++) {
-			cout << inventory.get(i)->getClassName() << endl;
-		}
-	}
 }
